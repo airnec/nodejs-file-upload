29618e2349959f33ba8cadd7edbf57ab7a040dc2
Necip Dirik <necipdirik@gmail.com>
2024-06-10 16:06:22 +0300

    parsing incoming files and storing them on the backend added

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const multer = require('multer');
 
-const upload = multer({});
+const storageConfig = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'images');
+  },
+
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storageConfig });
 
 const router = express.Router();
 
@@ -16,6 +26,11 @@ router.get('/new-user', function(req, res) {
 router.post('/profiles', upload.single('image'), function(req, res) {
   const uploadedImageFile = req.file;
   const userData = req.body;
+
+  console.log(uploadedImageFile);
+  console.log(userData);
+
+  res.redirect('/');
 });
 
 module.exports = router;
